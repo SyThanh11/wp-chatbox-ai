e2df9fd589a25397148587d01b55642b50a9f3a8
Thành Nguyễn <sythanhdev@gmail.com>
2025-06-02 20:21:57 +0700

    feature(wp-chatbot-ai): set up and define dir and load

---
+++ wp-chatbot-ai.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: WP Chatbot AI
+ * Plugin URI:
+ * Description: Plugin chatbot AI cho WordPress.
+ * Version: 1.0
+ * Author: LGL-AI
+ * Author URI: https://lgl-ai.com
+ * Text Domain: wp-chatbot-ai
+ */
+
+ defined('ABSPATH') || exit;
+
+ // Định nghĩa các constant plugin
+ define('LGL_VERSION', '1.0.0');
+ define( 'LGL_PREFIX', 'lgl' );
+ define( 'LGL_CHATBOT_DOMAIN', 'wp-chatbot-ai' );
+ define( 'WPCB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+ define( 'WPCB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+ require_once WPCB_PLUGIN_DIR . 'includes/class-loader.php';
+
+ add_action('plugin_loaded', function(): void {
+    WPCP_Loader::init;
+ });
+?>
